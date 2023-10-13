64127cf30bf09f1bb4cfa814d1cdf2448ab881e3
zjaad <zjaadani@gmail.com>
2023-10-12 18:31:27 -0700

    reading data from a json file and creating objects code, and some small changes on base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,41 +2,45 @@
 """ This module contains the mean model of all classes in this project"""
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """ This class is the model of all class"""
 
     def __init__(self, *arg, **kwargs):
         """This method initializes a new BaseModel instance."""
+        dateFormat = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         if kwargs:
             for k, value in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    value = datetime.fromisoformat(value)
-                if k != "__class__":
+                    setattr(self, k, datetime.strptime(value, date_format))
+                else:
+                # the k !== "created_at" or k !=="updated_at" case
                     setattr(self, k, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """This method updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
+        model.storage.save()
 
     def to_dict(self):
         """This method returns a dictionary containing
         all keys/values  of __dict__ of  the instance"""
+        className = self.__class__.__name__
         dict_to_return = self.__dict__.copy()
-        dict_to_return['__class__'] = self.__class__.__name__
+        dict_to_return['__class__'] = className
         dict_to_return['updated_at'] = self.updated_at.isoformat()
         dict_to_return['created_at'] = self.created_at.isoformat()
 
-        return dict_to_return
+        return (dict_to_return)
 
     def __str__(self):
         """This is the Tostring method"""
         return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__
         )
-
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 """This method contains the model for managing File Storage"""
 import json
 import os
-
+from models.base.model import BaseModel
 
 class FileStorage:
     """This class serializes instances to a JSON file
@@ -19,13 +19,37 @@ class FileStorage:
     def new(self, obj):
         """This method sets in __objects the obj with key <obj
         class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
+    
 
     def save(self):
         """This method serializes __objects to the
         JSON file (path: __file_path)"""
+        SerializeObj = {}
+        for key, obj in self.__objects.items():
+            SerializeObj[key] = obj.to.dict()
+        with open(self.__file_path, 'w', encoding= 'utf-8') as file:
+            json.dump(SerializeObj, file, indent= 4)
 
 
     def reload(self):
         """This method deserializes the JSON file to __objects"""
-
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                #loading the Json data from the file
+                ObjDic = json.load(file)
+                for key, val in ObjDic.items():
+                    #Extracting classname form dict
+                    ClassName = val.get("__class__")
+                    if ClassName:
+                        #it removes the class from the dict
+                        del val["__class__"]
+                        ObjClass= class_mapping.get(ClassName)
+                        if ObjClass:
+                            obj = ObjClass(**val)
+                            #adding the new obj to storage
+                            self.new(obj)
+        except FileNotFoundError:
+            return
