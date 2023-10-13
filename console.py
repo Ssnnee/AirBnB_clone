b37773b95cb78b8ac0b27502d34d0e3e116094fa
zjaad <zjaadani@gmail.com>
2023-10-12 19:06:49 -0700

    console.py file: adding do_creat

--- console.py
+++ console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Console Module. """
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -19,6 +22,24 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ empty line """
         pass
+
+    def do_create(self, arg):
+        """creating new instance of BaseModel"""
+        argSpl = arg.split()
+        if len(argSpl) == 0:
+            print("** class name missing **")
+            return
+        elif argSpl[0] not in BaseModel.___subclasses__():
+            print("** class doesn't exist **")
+            return
+        else:
+            ni= BaseModel.__classes[argSpl]()
+            ni.save()
+            print(ni.id)
+
+####
+
+
 
 
 if __name__ == '__main__':
